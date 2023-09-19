be41227cd2261d697ca4ae48b2e6a7cf79ff61e6
Who <blacklistuser420@gmail.com>
2023-09-19 09:46:01 +0000

    update

--- client/src/pages/profile/Profile.page.jsx
+++ client/src/pages/profile/Profile.page.jsx
@@ -4,6 +4,7 @@ import { Context } from "contexts/AppContexts";
 import { PROFILE } from "contexts/actions";
 import "./styles.css";
 import { Usericon } from "assets";
+import { Input } from "components";
 const body = document.body;
 const Profile = () => {
   const { color, changeColor, user } = useContext(Context);
@@ -19,12 +20,15 @@ const Profile = () => {
   return (
     <div className="user_container">
       <div className="user_img">
-        <Usericon width={180} height={180} stroke="red" />
+        <Usericon width={180} height={180}  />
       </div>
       <div className="user_info">
-        <input type="text" placeholder={user.name} disabled={false} />
-        <input type="email" placeholder={user.email} disabled={false} />
-        <input type="password" placeholder={user.password} disabled={false} />
+        <form>
+          <input type="text" placeholder={user.name} disabled={false} />
+          <input type="email" placeholder={user.email} disabled={false} />
+          <input type="password" placeholder={user.password} disabled={false} />
+        </form>
+        
       </div>
     </div>
   );
