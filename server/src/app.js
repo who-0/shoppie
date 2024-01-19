1d16518e436f7d7dc50db0ced7dc869d15bd9225
Who <blacklistuser420@gmail.com>
2024-01-19 09:22:05 +0000

    fix error

--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,12 @@ const morgan = require("morgan");
 const cookie = require("cookie-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const passport = require("passport");
-const session = require("express-session");
 const path = require("path");
 const api = require("./routes");
 const {
   errorHandlerMiddleware,
   notFoundMiddleware,
 } = require("./middlewares/index.js");
-const GoogleAuth = require("./config/Google");
 
 const app = express();
 
@@ -31,27 +28,9 @@ app.use(
 app.use(helmet());
 app.use(express.json());
 app.use(cookie());
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.SECRET_KEY,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-//!Oauth
-GoogleAuth(passport);
 
 //!Routes
 app.use("/api/v1", api);
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "test.html"));
-});
-app.get("/error", (req, res) => {
-  res.send("Can't Login with Google");
-});
 
 //!middleware
 app.use(errorHandlerMiddleware);
--- server/src/routes/auth.router.js
+++ server/src/routes/auth.router.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const passport = require("passport");
 const { signupController, loginController } = require("../controllers");
 const authRouter = express.Router();
 
