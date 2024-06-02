8469cad72bc23238e25ddc194fc080370c185364
Who <blacklistuser420@gmail.com>
2024-06-02 21:49:33 +0630

    change to v2

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -4,7 +4,6 @@ const secret = process.env.SECRET_KEY;
 
 const checkAuth = (req, res, next) => {
   const { token } = req.cookies;
-  console.log(req.cookies);
   if (!token) {
     throw new UnAuthenticatedError("Authentication Invalid");
   }
